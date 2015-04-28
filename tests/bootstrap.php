36bdb22785779f1a896e4d96a14a4296fe72c36d
Lymke <mickael.rolo@gmail.com>
2015-04-28 14:48:37 +0200

    Tests for database are ready to use

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,8 +19,14 @@ require_once 'Zend/Loader/Autoloader.php';
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->registerNamespace("App_");
 
+require_once 'Zend/Loader/Autoloader/Resource.php';
+$resources = new Zend_Loader_Autoloader_Resource(array(
+    'namespace' => 'App',
+    'basePath' => APPLICATION_PATH
+));
 
-
+$resources->addResourceType('form','forms','Form');
+$resources->addResourceType('model','models','Model');
 
 //BDD
 $config = new Zend_Config_Ini(APPLICATION_PATH.'/configs/db.ini',APPLICATION_ENV);
@@ -29,6 +35,7 @@ try {
     $db->query('SET NAMES UTF8');      // Pour mettre les donnees de la base en UTF-8
     $db->getConnection();
     Zend_Registry::set('db', $db);
+    Zend_Db_Table_Abstract::setDefaultAdapter($db);
 } catch (Exception $e) {
     exit($e->getMessage());
-} Zend_Db_Table_Abstract::setDefaultAdapter($db);
+}
